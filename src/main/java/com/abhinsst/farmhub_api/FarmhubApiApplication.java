c13dc14e0a998b87b38eb4175753611f4a07ad51
anand9773 <aaanand9773@gmail.com>
2024-08-26 12:15:23 +0530

    initial project structure created

---
+++ src/main/java/com/abhinsst/farmhub_api/FarmhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.abhinsst.farmhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmhubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinsst/farmhub_api/FarmhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinsst.farmhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
